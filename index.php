6505e081be418186af4b92fdd01543e73a7cd4fb
kazuki19992 <kushida98@gmail.com>
2019-04-25 10:27:15 +0900

    掲示板のコアです。作りました。

--- index.php
+++ index.php
@@ -1,17 +1,89 @@
+<!-- HTMLをPHPとして認識させる -->
+<FilesMatch "\.html$">
+    AddType application/x-httpd-php.html
+</FilesMatch>
+
+<!-- PHPの記述を行う -->
+
+<?php
+    //掲示板リスト用PHP
+
+    //データベースとの接続
+    $db = new PDO("sqlite:db/list.db");
+
+    //スレッド件数の取得
+    $thread = $db -> query("SELECT COUNT(*) FROM list");
+    $listcount = $thread -> fetchColumn();
+    $thread -> closeCursor();
+
+    //掲示板が存在した場合リンクを表示
+    if ($listcount > 0){
+        //SQLを使用し、投稿順に並べる
+        $thread = $db -> query("SELECT * FROM list WHERE flg = 0 ORDER BY updtime DESC");
+        $list = $thread -> fetchAll(PDO::FETCH_ASSOC);
+        
+        //変数宣言
+        $i = 1;
+        $html = "";
+
+        foreach ($list as $row) {
+            //リストの生成
+            $html .= "<a href='bbs.php?dbname={$row["dbname"]}
+            $dbname_kana={$row["dbname_kana"]}'>".$i." : ".$row["dbname_kana"].
+            "(".$row['posts'].") ".$row['updtime']."</a>".
+            "<a href='del.php?dbname{$row["dbname"]}'>削除する</a><br><br>";
+            
+            $i++;
+        }
+    }
+    $db = null;
+
+    //掲示板のスレッド数が10以下の場合、新規スレッド作成フォームを表示
+    if($listcount <= 100){
+        $listcount ++;
+        $newdbname = "bbs".$listcount;
+        
+        //ヒアドキュメントでフォームを出力する
+        $build = <<< EOF
+        
+        <form action="build.php" method="post">
+        <input type="hidden" name="newdbname" value="{$newdbname}">
+        名称 : <input type="text" name="newdbname_kana" value="">
+        <input type="submit" name="newbbs" value="スレ立てする！">
+        </form>
+        
+EOF;
+
+//終端ID前にインデントを入れるとうまくいかないため
+        
+    }else{
+        //スレッド数が規定数以上になった場合、エラーメッセージを表示
+        $build = "Opps! これ以上掲示板は作れません。";
+    }
+
+?>
+
+<!--以降 HTMLの記述へ -->
+
 <!DOCTYPE html>
-<meta charset="UTF-8">
-<title>掲示板</title>
-<h1>掲示板</h1>
-<section>
-    <h2>新規投稿</h2>
-    <!-- -->
-    <form action="" method="post">
-        名前：<input type="text" name="name" value=""><br>
-        本文：<input type="text" name="text" value=""><br>
-        <button type="submit">投稿</button>
-    </form>
-</section>
-<section>
-    <h2>投稿一覧</h2>
-    <p>投稿はまだありません</p>
-</section>
+<html lang="ja">
+    <head>
+        <title>【学内専用】BBS</title>
+        
+        <meta charset="utf-8">
+        <meta name"viewport" content="initial-scale=1.0">
+        <link rel="stylesheet" href="style.css" media="all">
+
+    </head>
+    <body>
+        <h2 style="color:#ff0000;">【学内専用】BBS</h1>
+        
+        <!-- 作成日の記述 -->
+        <script>
+            document.write("作成日：" + document.lastModified);
+        </script>
+        
+        <?php echo $html; ?>
+        <?php echo $build; ?>
+    </body>
+</html>
