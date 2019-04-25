704f3caba2b06de91187bc5f8bcb938482615a94
kazuki19992 <kushida98@gmail.com>
2019-04-25 18:24:56 +0900

    no message

--- bbs.php
+++ bbs.php
@@ -30,6 +30,47 @@ if ($page == "") {
 }
 
 //ページ移動のためのリンクを表示
+$link = "";
+for ($i=1; $i<=$pcount; $i++) {
+  $link .= "<a href=bbs.php?dbname=".$dbname."&dbname_kana=".$dbname_kana."&page=".$i.">".$i."</a>&nbsp;&nbsp;";
+}
+
+//ページ表示
+$stmt = $db->query("SELECT * FROM bbs LIMIT ($page-1)*$hyouji, $hyouji");
+$html = "<h2 style='color:#ff0000;'>".$dbname_kana."</h2>";
+$list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+foreach ($list as $row) {
+  $html .= $row["id"]." : ".$row["hiduke"]."<br>";
+  $html .= "<div id='commnet'>".$row["comment"]."</div><br><br>";
+}
+
+//コメントにアンカーがあればアンカーに変換する
+$html = anchor($dbname, $html);
+$db = null;
+
+// 総件数が1000件未満であれば投稿画面を表示する
+if ($kensu < 1000) {
+    $form = <<< EOF
+    <form action="ins.php" method="post">
+    <input type="hidden" name="dbname" value="{$dbname}">
+    投稿内容:<br>
+    <textarea name="comment" cols=100 rows=10></textarea><br>
+    <input type="submit" name="ins" value="送信">
+    </form>
+    <br>
+EOF;
+
+} else {
+    $form = "1000件以上は書き込めません。<br><br>";
+}
+
+  // コメント中にアンカーがあればアンカーリンクを付与する
+function anchor($dbname, $comment) {
+    $pattern = "/&gt;&gt;([0-9])+/";
+    $replace = '<a href="link.php?dbname='.$dbname.'&num=$0">$0</a>';
+    $comment = preg_replace($pattern, $replace, $comment);
+    return $comment;
+}
 
 
 ?>
